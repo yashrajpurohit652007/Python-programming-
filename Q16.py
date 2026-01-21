b4d490357169a98bec4de90863fed38f7a4adb8b
yashrajpurohit652007 <yashrajpurohit652007@gmail.com>
2026-01-21 09:05:20 +0530

    Create Q16.py

---
+++ Q16.py
@@ -0,0 +1,6 @@
+ 16. Simple Interest (I = PRN/100)
+P = float(input("Enter Principal amount (P): "))
+R = float(input("Enter Rate of interest (R): "))
+N = float(input("Enter Number of years (N): "))
+interest = (P * R * N) / 100
+print("Simple Interest:", interest)
