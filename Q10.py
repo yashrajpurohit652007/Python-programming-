93fec9d960f28ccc37fa0e184455ab7fd428e546
yashrajpurohit652007 <yashrajpurohit652007@gmail.com>
2026-01-21 08:51:56 +0530

    Create Q10.py

---
+++ Q10.py
@@ -0,0 +1,4 @@
+input_dollars = float(input("Enter dollars to convert to pounds: "))
+temp_rupees = input_dollars * 48  # Convert dollar to rs
+pounds = temp_rupees / 70         # Convert rs to pound
+print("Amount in Pounds:", pounds)
