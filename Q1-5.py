ce0930d0ff8bc11996d991e676704b82f4d7bded
yashrajpurohit652007 <yashrajpurohit652007@gmail.com>
2026-01-21 08:47:58 +0530

    Create Q1-5.py

---
+++ Q1-5.py
@@ -0,0 +1,20 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# 1. Add
+print("Addition:", num1 + num2)
+
+# 2. Subtract
+print("Subtraction:", num1 - num2)
+
+# 3. Multiply
+print("Multiplication:", num1 * num2)
+
+# 4. Divide
+print("Division:", num1 / num2)
+
+# 5. All operations in one line
+print(f"Sum: {num1+num2}, Diff: {num1-num2}, Mul: {num1*num2}, Div: {num1/num2}")
+
+
